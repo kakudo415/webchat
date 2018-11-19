412c8a8052aa9f4c6965909468e1c3135a1f238a
kakudo415 <kakudo415@gmail.com>
2018-11-19 15:56:00 +0900

    desing の発火ポイントを変更

--- public/script.js
+++ public/script.js
@@ -49,19 +49,17 @@ const nowTime = () => {
 
 connect('wss://kakudo.app/webchat/ws/');
 
-submitButton.onmouseover = () => {
+messageInput.oninput = () => {
   if (messageInput.value.length > 0) {
     submitButton.style.color = '#FFF';
     submitButton.style.background = '#4CAF50';
     submitButton.style.cursor = 'pointer';
-  };
+  } else {
+    submitButton.style.color = '#000';
+    submitButton.style.background = '#0002';
+    submitButton.style.cursor = 'default';
+  }
 }
-
-submitButton.onmouseout = () => {
-  submitButton.style.color = '#000';
-  submitButton.style.background = '#0002';
-  submitButton.style.cursor = 'default';
-};
 
 submitButton.onclick = () => {
   if (messageInput.value.length === 0) {
